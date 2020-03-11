b1adb5a34a32925d752f0bb024633bcbbe51f8eb
Logan Garay <garay.logan@gmail.com>
2020-03-10 23:53:42 -0600

    got animation on loading page working

--- wwwsrc/src/views/LoadingScreens/LoadingNew.vue
+++ wwwsrc/src/views/LoadingScreens/LoadingNew.vue
@@ -38,29 +38,32 @@ img {
   0% {
     transform: translate(0px);
   }
+  10% {
+    transform: translate(350px);
+  }
   20% {
-    transform: translate(800px);
+    transform: translate(700px);
+  }
+  30% {
+    transform: translate(1050px);
   }
   40% {
-    transform: translate(1200px);
+    transform: translate(1400px);
+  }
+  50% {
+    transform: translate(1800px);
   }
   60% {
-    transform: translate(800px);
+    transform: translate(1400px);
+  }
+  70% {
+    transform: translate(1050px);
   }
   80% {
-    transform: translate(400px);
+    transform: translate(700px);
   }
-  100% {
-    transform: translate(0);
-  }
-  100% {
-    transform: translate(0);
-  }
-  100% {
-    transform: translate(0);
-  }
-  100% {
-    transform: translate(0);
+  90% {
+    transform: translate(350px);
   }
   100% {
     transform: translate(0);
