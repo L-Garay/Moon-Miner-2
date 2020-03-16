b9e74668e9f40e30e0a0155157617b4d31da8e95
Logan Garay <garay.logan@gmail.com>
2020-03-15 21:27:43 -0600

    added test table to db

--- db-setup.sql
+++ db-setup.sql
@@ -1,7 +1,7 @@
 USE moonminer2;
 
-CREATE TABLE testing {
-  id int NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-}
+-- CREATE TABLE testing (
+--   id int NOT NULL AUTO_INCREMENT,
+--   name VARCHAR(255) NOT NULL,
+--   PRIMARY KEY (id)
+-- )
