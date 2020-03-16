58351e22cd6689896f76c47d10676c2aea96e8cf
Logan Garay <garay.logan@gmail.com>
2020-03-15 21:24:58 -0600

    working on backend

--- db-setup.sql
+++ db-setup.sql
@@ -1,2 +1,7 @@
 USE moonminer2;
 
+CREATE TABLE testing {
+  id int NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+}
--- wwwsrc/src/store/index.js
+++ wwwsrc/src/store/index.js
@@ -1,18 +1,27 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+let baseUrl = location.host.includes("localhost")
+  ? "https://localhost:5001/"
+  : "/";
+
+let api = axios.create({
+  baseURL: baseUrl + "api/",
+  timeout: 3000
+});
 
 export default new Vuex.Store({
   state: {
+    game: {}
   },
-  mutations: {
-  },
+  mutations: {},
   actions: {
-    async createGame({commit, dispatch}, newGame) {
-      await 
+    async createGame({ commit, dispatch }, newGame) {
+      await api;
     }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
