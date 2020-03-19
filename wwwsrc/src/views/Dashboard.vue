38a9000c7335014de9a940ecea75c29422c07a4f
Logan Garay <garay.logan@gmail.com>
2020-03-18 22:44:11 -0600

    got profile section of dash set up for now

--- wwwsrc/src/views/Dashboard.vue
+++ wwwsrc/src/views/Dashboard.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="dashboard container-fluid">
     <div class="row side">
-      <div class="col-7">
-        <h1>Profile Section</h1>
+      <div class="col-7" id="profile">
+        <div class="topProfile">
+          <div>
+            <img :src="game.profileImg" alt="should just be text" />
+          </div>
+          <div>
+            <h3>{{game.playerName}}</h3>
+            <p>This will be player xp</p>
+          </div>
+        </div>
+        <div class="seperator"></div>
+        <div class="bottomProfile">
+          <p>Player Money: {{game.playerMoney}}</p>
+          <p>Player Resource1:</p>
+          <p>Player Resource2:</p>
+          <p>Player Resource3:</p>
+          <p>Player Resource4:</p>
+          <p>Current Player Tool:</p>
+          <p>Player Energy:</p>
+        </div>
       </div>
       <div class="col-3">
         <button>Upgrades</button>
@@ -23,6 +41,9 @@
 <script>
 export default {
   name: "Dashboard",
+  data() {
+    return {};
+  },
   mounted() {
     this.$store.dispatch("getGame");
   },
@@ -35,6 +56,7 @@ export default {
 </script>
 
 <style scoped>
+/* Overall page layout */
 .dashboard {
   display: flex;
   height: 100vh;
@@ -49,5 +71,37 @@ export default {
 }
 .middle {
   width: 50vw;
+}
+
+/* Individual sections */
+
+/* Profile Section */
+.col-7#profile {
+  border: solid 1pt black;
+  width: 350px;
+  max-width: 350px !important;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+.topProfile {
+  display: flex;
+  align-items: center;
+  text-align: start;
+}
+.topProfile img {
+  height: 150px;
+  width: 150px;
+  margin-right: 8px;
+}
+.seperator {
+  width: 75%;
+  height: 1px;
+  border-top: 3px solid red;
+}
+.bottomProfile {
+  text-align: start;
+  align-self: flex-start;
 }
 </style>
