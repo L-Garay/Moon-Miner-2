92735c616e19a65f65b34e62e9f673b1f4ef5e9f
Logan Garay <garay.logan@gmail.com>
2020-03-18 21:33:55 -0600

    working on dahsboard view

--- wwwsrc/src/views/Dashboard.vue
+++ wwwsrc/src/views/Dashboard.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="dashboard container-fluid">
-    <div class="row">
-      <div class="col-12">
-        <h1>Welcome to your dashboard</h1>
-        {{game}}
+    <div class="row side">
+      <div class="col-7">
+        <h1>Profile Section</h1>
       </div>
+      <div class="col-3">
+        <button>Upgrades</button>
+      </div>
+    </div>
+    <div class="row middle">
+      <div class="col-12">
+        <h3>Planet Area</h3>
+      </div>
+    </div>
+    <div class="row side">
+      <div class="col-4">Market tab</div>
+      <div class="col-4">Stats tab</div>
     </div>
   </div>
 </template>
@@ -23,5 +34,20 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.dashboard {
+  display: flex;
+  height: 100vh;
+}
+.row {
+  flex-direction: column;
+  align-content: center;
+}
+.side {
+  width: 25vw;
+  justify-content: space-around;
+}
+.middle {
+  width: 50vw;
+}
 </style>
