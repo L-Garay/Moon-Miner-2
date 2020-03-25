dd304d288c03a8382994a348c81903ce4bcb4916
Logan Garay <garay.logan@gmail.com>
2020-03-24 23:01:48 -0600

    created planets table, filled with 4 test planets

--- db-setup.sql
+++ db-setup.sql
@@ -6,10 +6,27 @@ USE moonminer2;
 --   PRIMARY KEY (id)
 -- )
 
-CREATE TABLE testgame (
-  id int NOT NULL AUTO_INCREMENT,
-  playerName VARCHAR(255) NOT NULL,
-  profileImg VARCHAR(255) NOT NULL,
-  playerMoney int DEFAULT 0,
-  PRIMARY KEY (id)
-)
+-- CREATE TABLE testgame (
+--   id int NOT NULL AUTO_INCREMENT,
+--   playerName VARCHAR(255) NOT NULL,
+--   profileImg VARCHAR(255) NOT NULL,
+--   playerMoney int DEFAULT 0,
+--   PRIMARY KEY (id)
+-- )
+
+-- CREATE Table testplanets (
+--   id int NOT NULL AUTO_INCREMENT,
+--   planetName VARCHAR(255) NOT NULL,
+--   moneyNeeded int NOt NULL,
+--   expiditionCost int NOT NULL,
+--   resource1 int NOT NULL,
+--   resource2 int NOT NULL,
+--   resource3 int NOT NULL,
+--   resource4 int NOT NULL,
+--   PRIMARY KEY (id)
+-- )
+
+-- INSERT INTO testplanets (planetName, moneyNeeded, expiditionCost, resource1, resource2, resource3, resource4) VALUES ("Planet One", 0, 20, 1, 3, 5, 10);
+-- INSERT INTO testplanets (planetName, moneyNeeded, expiditionCost, resource1, resource2, resource3, resource4) VALUES ("Planet Two", 100, 35, 1, 10, 20, 30);
+-- INSERT INTO testplanets (planetName, moneyNeeded, expiditionCost, resource1, resource2, resource3, resource4) VALUES ("Planet Three", 250, 50, 10, 25, 40, 75);
+-- INSERT INTO testplanets (planetName, moneyNeeded, expiditionCost, resource1, resource2, resource3, resource4) VALUES ("Planet Four", 500, 75, 25, 50, 100, 150);
