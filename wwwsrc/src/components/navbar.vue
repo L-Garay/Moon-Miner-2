0f358482df7c2469065cadf23a540d694dcc7a54
Logan Garay <garay.logan@gmail.com>
2020-04-11 20:57:40 -0600

    still working on navbar

--- wwwsrc/src/components/navbar.vue
+++ wwwsrc/src/components/navbar.vue
@@ -1,5 +1,29 @@
 <template>
-  <nav></nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <h2 class="title">MoonMiner</h2>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdownMenuLink"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Main Menu</a>
+            <a class="dropdown-item" href="#">Dashboard</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -12,4 +36,10 @@ export default {
 </script>
 
 <style>
+.navbar {
+  display: flex;
+}
+.navbar .title {
+  color: gold;
+}
 </style>
