426f450b567b8d6b84f1388c1ca1101223df37b1
Logan Garay <garay.logan@gmail.com>
2020-09-15 14:21:42 -0600

    worked on home page sizing

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="home container-fluid">
     <div class="row">
-      <div class="col-6 offset-3">
-        <img src="../assets/Moons/moon1.png" alt="should be a moon" />
+      <div class="col-md-6 offset-md-3">
+        <img
+          src="../assets/Moons/moon1.png"
+          class="img-fluid"
+          alt="should be a moon"
+        />
       </div>
+    </div>
+    <div class="row">
       <div class="col-12 welcome">
         <h1>Welcome to Moon Miner 2.0</h1>
         <div class="buttons">
@@ -20,45 +26,62 @@
           </button>
         </div>
       </div>
-      <div
-        class="modal fade bd-example-modal-lg"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="myLargeModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog modal-dialog-centered modal-lg">
-          <div class="modal-content">
-            <div class="container-fluid">
-              <div class="row">
-                <div class="col-12">
-                  <h2>Select a saved game</h2>
-                </div>
-                <div class="col-8">
-                  <ol v-if="savedGames">
-                    <li v-for="game in games" :key="game.id">
-                      {{ game.playerName }}.....${{ game.playerMoney }}
-                      <button
-                        type="button"
-                        @click="loadGame(game.id)"
-                        class="btn loadBtn"
-                      >
-                        Load Game
-                      </button>
-                      <button
-                        type="button"
-                        @click="deleteGameById(game.id)"
-                        class="btn deleteBtn"
-                      >
-                        Delete Game
-                      </button>
-                    </li>
-                  </ol>
-                  <p v-else>
-                    Looks like you don't have any games saved yet, what are you
-                    waiting for? Get mining!
-                  </p>
-                </div>
+    </div>
+    <div
+      class="modal fade bd-example-modal-lg"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="myLargeModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-12">
+                <h2>Select a saved game</h2>
+              </div>
+              <div class="col-8 offset-2">
+                <ol v-if="savedGames">
+                  <div v-for="game in games" :key="game.id" class="p-1">
+                    <li>{{ game.playerName }}.....${{ game.playerMoney }}</li>
+                    <button
+                      type="button"
+                      @click="loadGame(game.id)"
+                      class="btn btn-sm loadBtn"
+                    >
+                      Load Game
+                    </button>
+                    <button
+                      type="button"
+                      @click="deleteGameById(game.id)"
+                      class="btn btn-sm deleteBtn"
+                    >
+                      Delete Game
+                    </button>
+                  </div>
+                  <!-- <li v-for="game in games" :key="game.id">
+                    {{ game.playerName }}.....${{ game.playerMoney }}
+                    <button
+                      type="button"
+                      @click="loadGame(game.id)"
+                      class="btn btn-sm loadBtn"
+                    >
+                      Load Game
+                    </button>
+                    <button
+                      type="button"
+                      @click="deleteGameById(game.id)"
+                      class="btn btn-sm deleteBtn"
+                    >
+                      Delete Game
+                    </button>
+                  </li> -->
+                </ol>
+                <p v-else>
+                  Looks like you don't have any games saved yet, what are you
+                  waiting for? Get mining!
+                </p>
               </div>
             </div>
           </div>
@@ -132,6 +155,22 @@ img {
   margin: 10pt;
   padding: 10pt;
 }
+
+.btn.loadBtn {
+  font-size: x-small;
+  margin: 1pt;
+  padding: 3pt;
+  color: white;
+  background-color: seagreen;
+}
+.btn.deleteBtn {
+  font-size: x-small;
+  margin: 1pt;
+  padding: 3pt;
+  color: white;
+  background-color: red;
+}
+
 li:hover {
   cursor: pointer;
 }
